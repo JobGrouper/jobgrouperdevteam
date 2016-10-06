358df2a06780ade9889037c93b84df3232d9e163
pavel <pavel_stepanenko_11@mail.ru>
2016-10-06 12:49:16 +0000

    edit help

--- resources/views/pages/help.blade.php
+++ resources/views/pages/help.blade.php
@@ -11,38 +11,96 @@
                     <div class="col-md-6">
                         <div class="faq">
                             <div class="faq_item">
-                                <p class="bold">Lorem ipsum dolor sit amet ea sit cetero ?</p>
+                                <p class="bold">What Is &#39;JobGrouper&#39;? How Does JobGrouper Work?</p>
                                 <span class="plus"></span>
-                                <div class="text_block">Lorem ipsum dolor sit amet, ea sit cetero assusamus, a idqran ende salutandi no per. Est eu pertinaciaen delacrue instructiol vel eu natum vedi idqran ende salutandi no per. Lorem ipsum dolor sit</div>
+                                <div class="text_block">JobGrouper is a subscription-based work marketplace, using a ‘group buy’ model similar to that
+
+                                                        used by Groupon. For an example, let’s say an editor wants to make $63,000 per year. All she would
+
+                                                        have to do is set her price at $35/month and require 150 buyers for her “unlimited editing” deal.
+
+                                                        Once 150 buyers have been achieved, buyers will be charged $25/month, and their term papers,
+
+                                                        resumes, and reports will look beautiful forever!</div>
                             </div>
                             <div class="faq_item">
-                                <p class="bold">Lorem ipsum dolor sit amet ea sit cetero ?</p>
+                                <p class="bold">That seems pretty cheap! What’s the catch?</p>
                                 <span class="plus"></span>
-                                <div class="text_block">Lorem ipsum dolor sit amet, ea sit cetero assusamus, a idqran ende salutandi no per. Est eu pertinaciaen delacrue instructiol vel eu natum vedi idqran ende salutandi no per. Lorem ipsum dolor sit</div>
+                                <div class="text_block">There’s no catch! Let’s say you begin scouring the web for a math tutor, and wow, he costs
+
+                                                        $60/hour! Convert that to the equivalent of a yearly full-time salary, and that’s $124,800/year!! Good
+
+                                                        luck finding a full-time teacher who makes that much. So, why does our math tutor charge so much?
+
+                                                        His rates are so high because he has to make up for the fact that his income is unstable, and he
+
+                                                        doesn’t have anywhere near 40 hours worth of clients in given week.
+
+                                                        JobGrouper, in contrast, gives workers stability by providing them with enough aggregated gigs to be
+
+                                                        equivalent to a full-time job, and the security of knowing that buyers are committing to a monthly
+
+                                                        subscription for their services.</div>
                             </div>
                             <div class="faq_item">
-                                <p class="bold">Lorem ipsum dolor sit amet ea sit cetero ?</p>
+                                <p class="bold">How can I leave a JobGrouper job?</p>
                                 <span class="plus"></span>
-                                <div class="text_block">Lorem ipsum dolor sit amet, ea sit cetero assusamus, a idqran ende salutandi no per. Est eu pertinaciaen delacrue instructiol vel eu natum vedi idqran ende salutandi no per. Lorem ipsum dolor sit</div>
+                                <div class="text_block">Both buyers and workers can leave a job at any time! If buyers don’t want to pay for another
+
+                                                        month of services, they can easily remove the job from within their respective buyer profiles. If a
+
+                                                        buyer leaves after they’ve already paid for a given month of services, they will be entitled to these
+
+                                                        services until the end of that billing cycle.
+
+                                                        Workers can similarly leave a job at any time, at the click of a button. After you request to leave the
+
+                                                        job, you will continue to work for two weeks while we seek a new worker. If for some reason, you
+
+                                                        need to discontinue working immediately, please reach out us at work@jobgrouper.com.
+
+                                                        Please note that leaving a job does not affect your ability to search for other jobs on JobGrouper.</div>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="faq floated">
                             <div class="faq_item">
-                                <p class="bold">Lorem ipsum dolor sit amet ea sit cetero ?</p>
+                                <p class="bold">How does JobGrouper make money?</p>
                                 <span class="plus"></span>
-                                <div class="text_block">Lorem ipsum dolor sit amet, ea sit cetero assusamus, a idqran ende salutandi no per. Est eu pertinaciaen delacrue instructiol vel eu natum vedi idqran ende salutandi no per. Lorem ipsum dolor sit</div>
+                                <div class="text_block">We charge buyers a 15% commission fee on each purchase. Since that is a common percentage
+
+                                                        for tipping, perhaps you can consider that as our ‘tip’ for bringing everyone together and lowering
+
+                                                        prices ;)</div>
                             </div>
                             <div class="faq_item">
-                                <p class="bold">Lorem ipsum dolor sit amet ea sit cetero ?</p>
+                                <p class="bold">What cities is JobGrouper in?</p>
                                 <span class="plus"></span>
-                                <div class="text_block">Lorem ipsum dolor sit amet, ea sit cetero assusamus, a idqran ende salutandi no per. Est eu pertinaciaen delacrue instructiol vel eu natum vedi idqran ende salutandi no per. Lorem ipsum dolor sit</div>
+                                <div class="text_block">JobGrouper is Global – We’re in New York, Beijing, and everywhere in between! That being said,
+
+                                                        some jobs listed on our platform are of a local nature; after all, you wouldn’t want to hire a handyman
+
+                                                        in London if you’d like to ensure clog-free pipes in Montréal… Nonetheless, we’re working hard to
+
+                                                        expand our local job offerings where ever we can!
+
+                                                        As you may have also noted, JobGrouper is available in both English and Chinese, but we are
+
+                                                        definitely planning to learn more languages!</div>
                             </div>
                             <div class="faq_item">
-                                <p class="bold">Lorem ipsum dolor sit amet ea sit cetero ?</p>
+                                <p class="bold">When does work begin on a job? When do payments take place?</p>
                                 <span class="plus"></span>
-                                <div class="text_block">Lorem ipsum dolor sit amet, ea sit cetero assusamus, a idqran ende salutandi no per. Est eu pertinaciaen delacrue instructiol vel eu natum vedi idqran ende salutandi no per. Lorem ipsum dolor sit</div>
+                                <div class="text_block">Work begins when a job has both a worker assigned to it and the required number of buyers.
+
+                                                        Until a worker is chosen, anyone may click “apply for this job” and submit their profiles for
+
+                                                        consideration by our team. If a worker has been selected and the required number of buyers have
+
+                                                        been reached, buyers’ accounts are then billed, and that date becomes the monthly billing date.
+
+                                                        Payments are disbursed to workers every two weeks.</div>
                             </div>
                         </div>
                     </div>
