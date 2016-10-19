2070bbb3fffb93843d91d084b6388a6a25dd54f6
pavel <pavel_stepanenko_11@mail.ru>
2016-10-19 14:55:25 +0000

    Markup info => StepanenkoP

--- resources/views/pages/jobs/job.blade.php
+++ resources/views/pages/jobs/job.blade.php
@@ -5,10 +5,25 @@
 @section('content')
 
     <div class="view">
-        <div class="alert_window">
+        <div class="alert_window month">
             <div class="alert_window__block">
                 <p>{!! $pageTexts[13] !!}</p>
+<<<<<<< HEAD
                 <h2>{{$job->monthly_salary}} USD</h2>
+=======
+                <h2>Salary: ${{$job->monthly_salary}} USD/month</h2>
+                <p>Monthly price of the purchase: 100 USD</p>
+                <p>Markup: 100*0,15 = 15 USD</p>
+                <p>Total: 115 USD</p>
+                <div class="cancel"></div>
+            </div>
+        </div>
+
+        <div class="alert_window year">
+            <div class="alert_window__block">
+                <p>{!! $pageTexts[13] !!}</p>
+                <h2>Salary: ${{$job->yearly_salary}} USD</h2>
+>>>>>>> fc96e4e... Markup info
                 <div class="cancel"></div>
             </div>
         </div>
@@ -129,7 +144,7 @@
                                 @if($jobPaid)
                                     <span class="approved">YOU HAVE ORDERED THIS JOB</span>
                                 @elseif($jobOrdered)
-                                    <span class="approved need" purchase={{$job->sales()->first()->id}}>PLEASE COMPLETE PAYMENT</span>
+				    <a href="/purchase/{{ $user_order_info->id }}"><span class="approved need">PLEASE COMPLETE PAYMENT</span></a>
                                 @else
                                     {{--Это было для сохранения карт и авто-оплат--}}
                                     {{--<a href="/purchase/{{$job->id}}"><button>Buy</button></a>--}}
