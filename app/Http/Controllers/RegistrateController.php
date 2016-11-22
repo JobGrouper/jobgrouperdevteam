561f09190e6f8f88e8c8e59d1c7805c5182a7955
Andrii <ovch2011@gmail.com>
2016-11-22 17:32:08 +0000

    Creating Stripe account on seller registration

--- app/Http/Controllers/RegistrateController.php
+++ app/Http/Controllers/RegistrateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\PaymentServices\StripeService;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -12,6 +13,7 @@ use App\UserSocialAccount;
 use App\ConfirmUsers;
 use Mail;
 use Auth;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Session;
 
 
@@ -34,7 +36,7 @@ class RegistrateController extends Controller
     }
 
 
-    public function register(Request $request, PaymentServiceInterface $psi)
+    public function register(Request $request, StripeService $ss)
     {
         $user = User::where('email', '=', $request->input('email'))->first();
         if ($user !== null) {
@@ -72,7 +74,42 @@ class RegistrateController extends Controller
                 $socialAccount->save();
             }
 
-            //отправляем ссылку с токеном пользователю
+            if($user->type == 'employee'){
+                //Creating Stripe Managed Account
+                $stripeAccountData = [
+                    "country" => $request->country,
+                    "email" => $request->email,
+                    "legal_entity" => [
+                        "address" => [
+                            "city" => $request->city,
+                            "line1" => $request->address,
+                            "postal_code" => $request->postal_code,
+                            "state" => $request->state
+                        ],
+                        "dob" => [
+                            "day" => $request->dob_day,
+                            "month" => $request->dob_month,
+                            "year" => $request->dob_year
+                        ],
+                        "first_name" => $request->first_name,
+                        "last_name" => $request->last_name,
+                        "ssn_last_4" => $request->ssn_last_4,
+                        "type" => 'individual',
+                    ],
+                    "tos_acceptance" => [
+                        "date" => time(),
+                        "ip" => $request->ip()
+                    ]
+                ];
+                $ss->createAccount($stripeAccountData, $user->id);
+            }
+            else{
+                //Creating Stripe Costumer  Account
+            }
+
+
+
+            //Sending confirmation mail to user
             Mail::send('emails.confirm',['token'=>$token],function($u) use ($user)
             {
                 $u->from('admin@jobgrouper.com');
