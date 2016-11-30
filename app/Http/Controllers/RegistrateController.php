7816bd8d4c81e0548a1ad618df547e5db0a36584
Andrii <ovch2011@gmail.com>
2016-11-30 17:07:01 +0000

    Validation in RegistrateController

--- app/Http/Controllers/RegistrateController.php
+++ app/Http/Controllers/RegistrateController.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\PaymentServices\StripeService;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Interfaces\PaymentServiceInterface;
-
 use App\User;
 use App\UserSocialAccount;
 use App\ConfirmUsers;
 use Mail;
 use Auth;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Session;
 
 
@@ -39,9 +35,38 @@ class RegistrateController extends Controller
     public function register(Request $request, PaymentServiceInterface $psi)
     {
 
-        $user = User::where('email', '=', $request->input('email'))->first();
+        /*$user = User::where('email', '=', $request->input('email'))->first();
         if ($user !== null) {
             die('User with this email already exist');
+        }*/
+
+        switch($request->user_type){
+            case 'buyer':
+                $this->validate($request, [
+                    'first_name' => 'required|min:2|max:255',
+                    'last_name' => 'required|min:2|max:255',
+                    'email' => 'required|unique:users|max:255',
+                    'password' => 'required|min:6|max:255',
+                ]);
+                break;
+            case 'employee':
+                $this->validate($request, [
+                    'first_name' => 'required|min:2|max:255',
+                    'last_name' => 'required|min:2|max:255',
+                    'email' => 'required|unique:users|max:255',
+                    'password' => 'required|min:6|max:255',
+                    'city' => 'required',
+                    'address' => 'required',
+                    'postal_code' => 'required',
+                    'state' => 'required',
+                    'dob_day' => 'required',
+                    'dob_month' => 'required',
+                    'dob_year' => 'required',
+                    'ssn_last_4' => 'required',
+                ]);
+                break;
+            default:
+                die('User type error');
         }
 
         //Creating new user
@@ -53,19 +78,27 @@ class RegistrateController extends Controller
             'password' => bcrypt($request->input('password')),
         ]);
 
+
         if(isset($user->id))
         {
             //Creating record for user email confirmation
             $email = $user->email;
-            $token = str_random(32);            //token for email confirmation
+            $token = str_random(32);
             $confirmUser = new ConfirmUsers;
             $confirmUser->email = $email;
             $confirmUser->token = $token;
             $confirmUser->save();
 
+            //Sending confirmation mail to user
+            /*Mail::send('emails.confirm',['token'=>$token],function($u) use ($user)
+            {
+                $u->from('admin@jobgrouper.com');
+                $u->to($user->email);
+                $u->subject('Confirm registration');
+            });*/
 
             //Check if registration began from creating the social account (fb / tw)
-            if($request->input('social_account_id')){
+            /*if($request->input('social_account_id')){
                 $socialAccount = UserSocialAccount::whereId($request->input('social_account_id'))
                     ->whereUserId(null)
                     ->first();
@@ -73,7 +106,7 @@ class RegistrateController extends Controller
                 //Associating user account with user social account
                 $socialAccount->user()->associate($user);
                 $socialAccount->save();
-            }
+            }*/
 
             if($user->user_type == 'employee'){
                 //Creating Stripe Managed Account
@@ -111,16 +144,6 @@ class RegistrateController extends Controller
                 ];
                 $psi->createCustomer($user, $costumerData);
             }
-
-
-
-            //Sending confirmation mail to user
-            Mail::send('emails.confirm',['token'=>$token],function($u) use ($user)
-            {
-                $u->from('admin@jobgrouper.com');
-                $u->to($user->email);
-                $u->subject('Confirm registration');
-            });
         }
         else {
             die('Something went wrong. Please try again later.');
