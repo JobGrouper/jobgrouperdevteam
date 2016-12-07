f50ba2176a06b19a2a68e7220a836e1677c1d7b5
Andrii <ovch2011@gmail.com>
2016-12-07 17:19:37 +0000

    Dynamic front end form for additional_verification created. Stripe account update feauture created. Added function for verif. documents uploading.

--- app/Http/Controllers/StripeVerificationRequestsController.php
+++ app/Http/Controllers/StripeVerificationRequestsController.php
@@ -2,26 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\PaymentServiceInterface;
 use App\StripeVerificationRequest;
 use Illuminate\Http\Request;
+use Session;
 
 use App\Http\Requests;
 
 class StripeVerificationRequestsController extends Controller
 {
-    public function edit(Request $request){
+    public function update(Request $request, PaymentServiceInterface $psi){
         $stripeVerificationRequest = StripeVerificationRequest::findOrFail($request->id);
 
-        return view('pages.account.stripe_verification_form', [
-            'fields_needed' => json_decode($stripeVerificationRequest->fields_needed, true),
-            'id' => $stripeVerificationRequest->id
-        ]);
-    }
+        $stripeAccountData = $request->stripeAccountData;
+        $stripeAccountData['tos_acceptance']['date'] = time();
+        //$stripeAccountData['tos_acceptance']['ip'] = $_SERVER['REMOTE_ADDR'];
+        $stripeAccountData['tos_acceptance']['ip'] = '89.252.17.119';   //for testing on local machine
 
-    public function update(Request $request){
-        //update stripe account
-        //check $stripeVerificationRequest->completed to true
-        $stripeVerificationRequest = StripeVerificationRequest::findOrFail($request->id);
-        dd($stripeVerificationRequest);
+        //If file of verification document was attached.
+        if ($request->hasFile('verification_document')) {
+            $fileName = md5(uniqid()).'.'.$request->verification_document->getClientOriginalExtension();
+            $request->file('verification_document')->move('temp/', $fileName);
+            $document_id = $psi->uploadDocument('temp/'.$fileName, $stripeVerificationRequest->managed_account_id);
+            unlink('temp/'.$fileName);  //remove temporary file
+            unset($request->verification_document);
+            $stripeAccountData['legal_entity']['verification']['document'] = $document_id;
+        }
+
+        $response = $psi->updateAccount($stripeVerificationRequest->managed_account_id, $stripeAccountData);
+
+        if($response){
+            $stripeVerificationRequest->completed = true;
+            $stripeVerificationRequest->save();
+            dd('Success'); //Redirect to "success page" will be here
+        }
+        else{
+            Session::flash('message_error', 'Unknown error.');
+            return redirect()->back();
+        }
     }
 }
