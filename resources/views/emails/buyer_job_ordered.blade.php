6e22415db470f4d6c1f334626be4d1920cb7cf22
i-nottage <inottage@yahoo.com>
2017-02-02 02:26:40 -0800

    added testsuite for email sending

--- resources/views/emails/buyer_job_ordered.blade.php
+++ resources/views/emails/buyer_job_ordered.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <p>You have successfully added {{ $job_name }} to your list of orders!</p>
 
-@if(!$employee_exist)
+@if(!$employee_exists)
     <p>You will notified when {{ $job_name }} is ready for you to confirm your order.</p>
 @endif
 @endsection
