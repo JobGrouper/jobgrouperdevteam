a68a8122f7e9b7c41fd1a59ba696afa5d5822bb0
i-nottage <inottage@yahoo.com>
2017-03-14 10:52:27 -0700

    email url fixes

--- resources/views/emails/buyer_order_confirmed.blade.php
+++ resources/views/emails/buyer_order_confirmed.blade.php
@@ -6,7 +6,7 @@
 @if($job->status == 'working')
 <p>Since this job is active, you can expect your first payment to be charged in a day or so.
 After that, payments will proceed monthly. If you wish to cancel your order at any time,
-visit your account's <a href="{{ Request::root() }}/my_orders">My Orders</a> page</p>
+visit your account's <a href="{{ env("SERVICE_APP_URL") }}/my_orders">My Orders</a> page</p>
 @else
 <p>Work will begin once the minimum number of buyers has been reached. We'll be sure to let you know
 when that happens. Until then, thanks for using JobGrouper!
