1e8a0fa69dd116c1c5b7d07317c8cd9ccdea1af8
i-nottage <inottage@yahoo.com>
2017-04-04 23:40:11 -0700

    removed a createPlan call from employee request controller

--- app/Jobs/StripeTransferPaid.php
+++ app/Jobs/StripeTransferPaid.php
@@ -23,7 +23,7 @@ class StripeTransferPaid extends Job implements ShouldQueue
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($event)
     {
         //
 	$this->event = $event;
