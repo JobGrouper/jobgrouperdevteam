ec95084b1d6ac356a6bb26999f52d28cac39adbf
i-nottage <inottage@yahoo.com>
2017-04-26 13:08:21 -0700

    added buyer name to seller_payment_successful email

--- resources/views/emails/seller_payment_successful.blade.php
+++ resources/views/emails/seller_payment_successful.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.email')
 
 @section('content')
-<p>Payment from one of your buyers was successful. The funds will be transferred to your account in two days time.</p>
+<p>{{ $buyer_name }}, one of your buyers, has paid this month's invoice. The funds will be transferred to your account in two days time.</p>
 @endsection
