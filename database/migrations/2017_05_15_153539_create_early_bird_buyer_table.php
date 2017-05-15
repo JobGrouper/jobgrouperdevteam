f166873aea4a2c38ab6778bd543e8ac53c868155
i-nottage <inottage@yahoo.com>
2017-05-15 16:13:11 -0700

    early-bird-buyer migration

---
+++ database/migrations/2017_05_15_153539_create_early_bird_buyer_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEarlyBirdBuyerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('early_bird_buyers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('employee_id')->unsigned();
+            $table->integer('job_id')->unsigned();
+            $table->enum('status', array('requested', 'denied', 'working', 'ended'))->default('requested');
+            $table->timestamps();
+
+	    // fks
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('employee_id')->references('id')->on('users');
+            $table->foreign('job_id')->references('id')->on('jobs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+	Schema::drop('early_bird_buyers');
+    }
+}
