11bef538f4b75df4a649534bb80b552c5f890770
i-nottage <inottage@yahoo.com>
2017-05-16 15:17:26 -0700

    created early bird controller and added methods

---
+++ app/Http/Controllers/EarlyBirdBuyerController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class EarlyBirdBuyerController extends Controller
+{
+
+	public function sendRequest() {
+
+	}
+
+	public function confirmRequest() {
+
+	}
+
+	public function cancelRequest() {
+
+	}
+
+	public function denyRequest() {
+
+	}
+
+	private function startEarlyBird() {
+
+	}
+
+	private function stopEarlyBird() {
+
+	}
+}
