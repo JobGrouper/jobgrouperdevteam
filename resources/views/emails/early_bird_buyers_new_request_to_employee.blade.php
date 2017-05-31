9f8f989dd14bd1b0dd6b493159038883d7b54f3d
i-nottage <inottage@yahoo.com>
2017-05-31 02:25:45 -0700

    email_fixes

--- resources/views/emails/early_bird_buyers_new_request_to_employee.blade.php
+++ resources/views/emails/early_bird_buyers_new_request_to_employee.blade.php
@@ -6,5 +6,5 @@ This means that, for the higher rate of ${{$job->early_bird_markup}} per month,
 this buyer has requested to start work now.</p>
 
 <p>You can decide if you would like to accept their request or deny it from 
-the <a href="{{env('APP_URL').'/job/'.$job->id}}">job's page</a></p>.
+the <a href="{{env('SERVICE_APP_URL').'/job/'.$job->id}}">job's page</a></p>.
 @endsection
--- resources/views/emails/early_bird_buyers_request_confirmed_to_employee.blade.php
+++ resources/views/emails/early_bird_buyers_request_confirmed_to_employee.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.email')
 
 @section('content')
-<p>A buyer of <a href="{{env('APP_URL').'/job/'.$job->id}}">{{$job->title}}</a> has paid for 
+<p>A buyer of <a href="{{env('SERVICE_APP_URL').'/job/'.$job->id}}">{{$job->title}}</a> has paid for 
 "early bird" access to your services. This means that, for the higher rate of 
 ${{ number_format($job->early_bird_markup, 2) }} per month, this buyer has requested to start work now. You may 
 communicate with your buyer by clicking the "message" link under his name on the Job page.</p>
 
 
 <p>Please note that this does not affect other buyers of your services for 
-<a href="{{env('APP_URL').'/job/'.$job->id}}">{{$job->title}}</a>, who will need to wait until 
+<a href="{{env('SERVICE_APP_URL').'/job/'.$job->id}}">{{$job->title}}</a>, who will need to wait until 
 the minimum number of buyers has been reached for work to begin. Other buyers may, however, 
 also purchase early bird services, and these early bird buyers will count toward the minimum 
 number of buyers required to begin work.</p>
